46cddf53d733d1cdf9e19265a7f3f2a0b16aff4c
Karnjj <karnjj@hotmail.com>
2019-12-04 13:57:54 +0700

    Update Login.js

--- openotog/src/Login.js
+++ openotog/src/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-//import { login } from './UserFunc'
+import { Redirect } from 'react-router-dom'
+import { login } from './UserFunc'
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
@@ -14,7 +15,8 @@ class Login extends Component {
     this.state = {
       username: '',
       password: '',
-      errors: {}
+      errors: {},
+      redirect: false
     }
 
     this.onChange = this.onChange.bind(this)
@@ -31,54 +33,57 @@ class Login extends Component {
       username: this.state.username,
       password: this.state.password
     }
-    /*
     login(user).then(res => {
       if (res) {
-        this.props.history.push(`/home`)
+        //console.log(res);
+        this.props.history.push("/main")
       }
     })
-    */
   }
-
+  
   render() {
+    var redirect = this.state.redirect;
+    if (redirect) {
+      return <Redirect to='/main'/>;
+    }
     return (
-      <Container>
-        <Row>
-          <div className="col-md-6 mt-5 mx-auto">
-            <form noValidate onSubmit={this.onSubmit}>
-              <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-              <div className="form-group">
-                <label htmlFor="username">Email address</label>
-                <input
-                  type="username"
-                  className="form-control"
-                  name="username"
-                  placeholder="Enter username"
-                  value={this.state.username}
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                  placeholder="Password"
-                  value={this.state.password}
-                  onChange={this.onChange}
-                />
-              </div>
-              <button
-                type="submit"
-                className="btn btn-lg btn-primary btn-block"
-              >
-                Sign in
-              </button>
-            </form>
-          </div>
-        </Row>
-      </Container>
+        <Container>
+            <Row>
+            <div className="col-md-6 mt-5 mx-auto">
+                <form noValidate onSubmit={this.onSubmit}>
+                <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+                <div className="form-group">
+                    <label htmlFor="username">Email address</label>
+                    <input
+                    type="username"
+                    className="form-control"
+                    name="username"
+                    placeholder="Enter username"
+                    value={this.state.username}
+                    onChange={this.onChange}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    placeholder="Password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                    />
+                </div>
+                <button
+                    type="submit"
+                    className="btn btn-lg btn-primary btn-block"
+                >
+                    Sign in
+                </button>
+                </form>
+            </div>
+            </Row>
+        </Container>
     )
   }
 }
